9986695d2ea46411b9d12f7227753b5885500f81
Azzeccagarbugli <f.coppola1998@gmail.com>
2016-12-24 23:16:12 +0100

    [ADD] Aggiunto piccolo script per augurarvi buon Natale

---
+++ buon_natale.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Piccolo script per augurare un Buon Natale a tutti quanti!
+
+echo -e "Inserisci il tuo nome... \c"
+read NomeVero    
+echo "Tantissimi auguri di buon Natale ${NomeVero^}, a te e a tutta la tua famiglia!"
